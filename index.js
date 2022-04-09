8b2c95a6cbf43ac34003925a7270ea6217166369
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-09 17:21:03 +0800

    add different routes

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const app = express()
 const PORT = 3000
 
 app.get('/', (req, res) => {
-    res.send('hello')
+    res.end('hello')
+})
+
+app.get('/messages', (req, res) => {
+    res.send('<h1>hello user</h1>')
+})
+
+app.post('/messages', (req, res) => {
+    console.log('updating messages');
 })
 
 app.listen(PORT, () => {
