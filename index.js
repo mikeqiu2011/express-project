6a43372c7906f9bdc85d3fec3696278811813f96
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-09 17:18:29 +0800

    respond hello when get home page

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const express = require('ex')
+const express = require('express')
+
+const app = express()
+
+const PORT = 3000
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+
+app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`);
+})
