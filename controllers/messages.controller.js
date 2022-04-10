cd3144a61846cf6433d54a7307a0efe907214fa1
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-10 09:59:38 +0800

    send file to client

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,9 +1,12 @@
+const path = require('path')
+
 function postMessages(req, res) {  // used named function instead of error easy for debug
     console.log('updating messages');
 }
 
 function getMessages(req, res) {
-    res.send('<h1>hello user</h1>')
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname, '..', 'public', 'cumulus.jpeg'))
 }
 
 module.exports = {
